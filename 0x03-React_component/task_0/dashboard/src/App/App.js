fc8a3750203f73ab850ecca7f7d681c5898a3036
MbaliFelecia <ramakgashambali@gmail.com>
2024-04-14 21:33:46 +0200

    Read me file

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -13,36 +13,34 @@ const listCourses = [
   { id: 2, name: "Webpack", credit: 20 },
   { id: 3, name: "React", credit: 40 },
 ];
+
 const listNotifications = [
   { id: 1, type: "default", value: "New course available" },
   { id: 2, type: "urgent", value: "New resume available" },
-  { id: 3, type: "urgent", html:{__html: getLatestNotification() }},
-]
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-	<Notifications listNotifications={listNotifications} />
-        <div className="App">
-            <Header />
-            <div className="App-body">
-              {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-            </div>
+  { id: 3, type: "urgent", html: getLatestNotification() },
+];
+
+function App(isLoggedIn) {
+  return (
+    <React.Fragment>
+      <div className="App">
+        <div className="heading-section">
+          <Notifications listNotifications={listNotifications} />
+          <Header />
         </div>
-        <div className="App-footer">
-          <Footer />
-        </div>
-      </React.Fragment>
-    );
-  }
+        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+        <Footer />
+      </div>
+    </React.Fragment>
+  );
 }
 
 App.defaultProps = {
-  isLoggedIn: false
-}
+  isLoggedIn: false,
+};
 
 App.propTypes = {
-  isLoggedIn: PropTypes.bool
-}
+  isLoggedIn: PropTypes.bool,
+};
 
 export default App;
